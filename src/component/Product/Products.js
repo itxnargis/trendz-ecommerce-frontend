f112e7eef52c29537818f5c517ee62a034114076
Nargis Khatun <itxnargiskhatun@gmail.com>
2024-07-04 12:38:34 +0530

    Feat - Add filters

--- src/component/Product/Products.js
+++ src/component/Product/Products.js
@@ -10,6 +10,8 @@ import { useAlert } from "react-alert";
 import Slider from "@material-ui/core/Slider";
 import Typography from "@material-ui/core/Typography";
 import MetaData from "../layout/metaData";
+import { FaFilter } from "react-icons/fa"; // Import the filter icon
+import Modal from '@material-ui/core/Modal'; // Import Modal component
 
 const categories = [
     "Laptop",
@@ -31,6 +33,7 @@ const Products = () => {
     const [price, setPrice] = useState([0, 25000]);
     const [category, setCategory] = useState("");
     const [ratings, setRatings] = useState(0);
+    const [open, setOpen] = useState(false); // State to handle modal visibility
 
     const { products, loading, error, productsCount, resultPerPage, filteredProductsCount } =
         useSelector((state) => state.products
@@ -46,6 +49,19 @@ const Products = () => {
         setPrice(newPrice);
     }
 
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const applyFilters = () => {
+        handleClose();
+        dispatch(getProduct(keyword, currentPage, price, category, ratings));
+    }
+
     useEffect(() => {
         if (error) {
             alert.error(error);
@@ -56,6 +72,48 @@ const Products = () => {
 
     let count = filteredProductsCount;
 
+    const filterContent = (
+        <div className="filterContent">
+            <Typography className="customFontSize">Price</Typography>
+            <Slider
+                value={price}
+                onChange={priceHandler}
+                valueLabelDisplay="auto"
+                aria-labelledby="range-slider"
+                min={0}
+                max={25000}
+            />
+            <Typography className="customFontSize">Categories</Typography>
+            <ul className="categoryBox">
+                {categories.map((category) => (
+                    <li
+                        className="category-link"
+                        key={category}
+                        onClick={() => setCategory(category)}
+                    >
+                        {category}
+                    </li>
+                ))}
+            </ul>
+            <fieldset>
+                <Typography component="legend">Ratings Above</Typography>
+                <Slider
+                    value={ratings}
+                    onChange={(e, newRating) => {
+                        setRatings(newRating);
+                    }}
+                    aria-labelledby="continuous-slider"
+                    min={0}
+                    max={5}
+                    valueLabelDisplay="auto"
+                />
+            </fieldset>
+            <div className="filterButtons">
+                <button onClick={applyFilters} className="applyButton">Done</button>
+                <button onClick={handleClose} className="cancelButton">Cancel</button>
+            </div>
+        </div>
+    );
 
     return (
         <Fragment>
@@ -65,6 +123,9 @@ const Products = () => {
                 <Fragment>
                     <MetaData title="PRODUCTS --- ECOMMERCE" />
                     <h2 className="productsHeading">Products</h2>
+                    <div className="filterIcon" onClick={handleOpen}>
+                        <FaFilter size={30} />
+                    </div>
                     <div className="products">
                         {products && products.map((product) => (
                             <Product key={product._id} product={product} />
@@ -73,7 +134,6 @@ const Products = () => {
 
                     <div className="filterBox">
                         <Typography className="customFontSize">Price</Typography>
-
                         <Slider
                             value={price}
                             onChange={priceHandler}
@@ -82,23 +142,18 @@ const Products = () => {
                             min={0}
                             max={25000}
                         />
-
                         <Typography className="customFontSize">Categories</Typography>
                         <ul className="categoryBox">
-                            {
-                                categories.map((category) => (
-                                    <li
-                                        className="category-link"
-                                        key={category}
-                                        onClick={() => setCategory(category)}
-                                    >
-                                        {category}
-                                    </li>
-                                ))
-                            }
+                            {categories.map((category) => (
+                                <li
+                                    className="category-link"
+                                    key={category}
+                                    onClick={() => setCategory(category)}
+                                >
+                                    {category}
+                                </li>
+                            ))}
                         </ul>
-
-
                         <fieldset>
                             <Typography component="legend">Ratings Above</Typography>
                             <Slider
@@ -112,30 +167,34 @@ const Products = () => {
                                 valueLabelDisplay="auto"
                             />
                         </fieldset>
-
                     </div>
 
-                    {
-                        resultPerPage < count && (
-
-                            <div className="PaginationBox">
-                                <Pagination
-                                    activepage={currentPage}
-                                    itemsCountPerPage={resultPerPage}
-                                    totalItemsCount={productsCount}
-                                    onChange={setCurrentPageNo}
-                                    nextPageText="Next"
-                                    prevPageText="Prev"
-                                    firstPageText="1st"
-                                    lastPageText="Last"
-                                    itemClass="page-item"
-                                    linkClass="page-link"
-                                    activeClass="pageItemActive"
-                                    activeLinkClass="pageLinkActive"
-                                />
-                            </div>
-                        )}
-
+                    {resultPerPage < count && (
+                        <div className="PaginationBox">
+                            <Pagination
+                                activePage={currentPage}
+                                itemsCountPerPage={resultPerPage}
+                                totalItemsCount={productsCount}
+                                onChange={setCurrentPageNo}
+                                nextPageText="Next"
+                                prevPageText="Prev"
+                                firstPageText="1st"
+                                lastPageText="Last"
+                                itemClass="page-item"
+                                linkClass="page-link"
+                                activeClass="pageItemActive"
+                                activeLinkClass="pageLinkActive"
+                            />
+                        </div>
+                    )}
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="simple-modal-title"
+                        aria-describedby="simple-modal-description"
+                    >
+                        {filterContent}
+                    </Modal>
                 </Fragment>
             )}
         </Fragment>
