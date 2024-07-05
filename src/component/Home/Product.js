c48a70ea4ca953687fcd01494eb545b4dca4a1a3
Nargis Khatun <itxnargiskhatun@gmail.com>
2024-07-05 16:02:54 +0530

    Feat - Image size

--- src/component/Home/Product.js
+++ src/component/Home/Product.js
@@ -12,14 +12,12 @@ const Product = ({ product }) => {
     return (
         <Link className="productCard" to={`/product/${product._id}`}>
             <img src={product.images[0].url} alt={product.name} />
-            <div className="details">
             <p>{product.name}</p>
             <div>
                 <Rating {...options} />{" "}
                 <span className="productCardSpan">({product.numOfReviews} Reviews)</span>
             </div>
             <span>{`Rs${product.price}`}</span>
-            </div>
         </Link>
     );
 };
