3fb8f0dd1cfb7050f46e3a3ddecf44b02a15fc63
itxnargis <itxnargiskhatun@gmail.com>
2024-07-20 19:11:50 +0530

    Fix: Header section

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,7 +3,7 @@ import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import Product from "./Product.js";
 import MetaData from "../layout/metaData.js";
-import Header from "../layout/Header/Header.js";
+import Header from "../layout/Header/Header";
 import { clearErrors, getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
 import Loader from "../layout/Loader/Loader";
--- src/component/layout/Header/Header.js
+++ src/component/layout/Header/Header.js
@@ -1,32 +1,71 @@
-import React from 'react';
-import Search from "../../Product/Search.js";
-import { FaBars, FaSearch, FaHeart, FaShoppingCart, FaAdn } from 'react-icons/fa';
+import React, { useState } from 'react';
+import Search from '../../Product/Search.js';
+import { FaBars, FaChevronCircleDown, FaSearch, FaHeart, FaShoppingCart, FaAdn, FaTimes } from 'react-icons/fa';
 import './Header.css';
-import logo from "../../../images/logo.png";
-import { Link } from "react-router-dom";
-import { FaFilter, FaUser } from "react-icons/fa";
+import logo from '../../../images/logo.png';
+import { Link } from 'react-router-dom';
+import { FaFilter, FaUser } from 'react-icons/fa';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='header'>
-      <div className='header-component'>
-        <Link to="#" className="bar-logo"><FaBars /></Link>
-        <div className='links'>
-        <Link to="/" className="link-details">Home</Link>
-        <Link to="/products" className="link-details">Projects</Link>
-        <Link to="/about" className="link-details">About</Link>
-        <Link to="/contact" className="link-details">Contact</Link>
-        </div>
-        <div className="social">
-        <Link to="/login" className="login-icon">
-          <FaUser />
-        </Link>
-        <Link to="/cart" className="cart-icon">
-         <FaShoppingCart />
-        </Link>
+    <header>
+      <div className='header'>
+        <div className='header-component'>
+          <Link to="#" className="bar-logo" onClick={toggleSidebar}><FaBars /></Link>
+          <div className='links'>
+            {/* These links will be hidden on small screens */}
+            <Link to="/" className="link-details">Home</Link>
+            <Link to="/products" className="link-details">Projects</Link>
+            <Link to="/about" className="link-details">About</Link>
+            <Link to="/contact" className="link-details">Contact</Link>
+          </div>
+          <div className="social">
+            <Link to="/login" className="login-icon">
+              <FaUser />
+            </Link>
+            <Link to="/cart" className="cart-icon">
+              <FaShoppingCart />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* Sidebar for small devices */}
+      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+        <div className='sidebar-content'>
+          <Link to="#" className="close-icon" onClick={toggleSidebar}><FaTimes /></Link>
+          <div className='sidebar-links'>
+            <Link to="/" className="sidebar-link">Home</Link>
+            <Link to="/products" className="sidebar-link">Projects</Link>
+            <Link to="/about" className="sidebar-link">About</Link>
+            <Link to="/contact" className="sidebar-link">Contact</Link>
+          </div>
+        </div>
+      </div>
+
+      <div className='header-bottom'>
+        <div className='header-bottom-box'>
+          <Link to="/filter" className="filter-logo">Sort By<FaChevronCircleDown /></Link>
+          <div className='search'>
+            <Search />
+          </div>
+          <div className="">
+            <Link to="/login" className="login-icon">
+              <FaUser />
+            </Link>
+            <Link to="/cart" className="cart-icon">
+              <FaShoppingCart />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
